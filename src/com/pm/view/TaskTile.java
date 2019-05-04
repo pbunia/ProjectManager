5f1a068cc2d7243a5222c15cd11f03636c7ba1e5
Szypr <szyprowski@outlook.com>
2019-05-04 17:21:50 +0200

    tasks can change finishStatus now

--- src/com/pm/view/TaskTile.java
+++ src/com/pm/view/TaskTile.java
@@ -2,27 +2,36 @@ package com.pm.view;
 
 import java.time.LocalDate;
 
+import com.pm.model.client.PMClient;
 import com.pm.model.task.Category;
 import com.pm.model.task.Priority;
+import com.pm.model.task.Task;
 
-import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
 public class TaskTile extends Pane {
 
 	private Long id;
+	private String userId;
 	private String groupId;
+	private String title;
+	private String comment;
+	private Category category;
+	private LocalDate createDate;
+	private LocalDate finishDate;
+	private Priority priority;
+	
+	
 
-//	private Stage stage= new Stage();
+	
+
 	private HBox hBox1 = new HBox();
 	private HBox hBox2 = new HBox();
 	private VBox vBox = new VBox();
-//	private Scene scene= new Scene(hBox2);
 
 	private Label categoryLabel;
 	private Label createDateLabel;
@@ -36,15 +45,20 @@ public class TaskTile extends Pane {
 	public TaskTile(Long id, String userId, String groupId, String title, String comment, Category category,
 			LocalDate createDate, LocalDate finishDate, Priority priority, boolean finishStatus) {
 		this.id = id;
+		this.userId=userId;
 		this.groupId = groupId;
+		this.title = title;
+		this.comment = comment;
+		this.category = category;
+		this.createDate = createDate;
+		this.finishDate = finishDate;
+		this.priority = priority;
+		
 
 		setHBox1(category, createDate, finishDate, priority);
 		setVBox(title, userId, comment);
 		setHBox2(finishStatus);
 		getChildren().add(hBox2);
-//		stage.setScene(scene);
-//		stage.show();
-
 	}
 
 	public void setHBox1(Category category, LocalDate createDate, LocalDate finishDate, Priority priority) {
@@ -70,17 +84,24 @@ public class TaskTile extends Pane {
 	public void setHBox2(boolean finishStatus) {
 		finishCB = new CheckBox();
 		finishCB.setSelected(finishStatus);
+		finishCB.setOnAction(e->changeStatus(finishCB.isSelected()));
 
 		hBox2.getChildren().addAll(finishCB, vBox);
 		hBox2.setSpacing(20);
 	}
 
-//	public Long getId() {
-//		return id;
-//	}
+	public Long getTheId() {
+		return id;
+	}
 
 	public String getGroupId() {
 		return groupId;
+	}
+	
+	public void changeStatus(boolean finishStatus) {
+		PMClient client = new PMClient();
+		client.putTask(new Task(id, userId, groupId, title, comment, category,
+			createDate, finishDate, priority, finishStatus));
 	}
 
 }
