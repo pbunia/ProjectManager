4f1777b633981b294f77e577d94642af38830651
irokes11 <irokes1@gmail.com>
2019-06-08 22:29:04 +0200

    commented Test methods

--- src/com/pm/test/Tests.java
+++ src/com/pm/test/Tests.java
@@ -12,11 +12,19 @@ import com.pm.model.client.PMClient;
 import com.pm.model.task.Category;
 import com.pm.model.task.Priority;
 import com.pm.model.task.Task;
-
+/**
+ * This is a testing class consisting of 7 tests 
+ * @author Mateusz Szyprowki
+ *
+ */
 public class Tests {
 
 	Task taskTest = new Task("U_Test", "Project_Test", "Title_Test", "d", Category.DOM, LocalDate.now(), LocalDate.now().plusDays(10), Priority.WYSOKI,
 			true);
+	
+	/**
+	 * Method check DELETE method. Invokes tasks and those that match U_Test are deleted
+	 */
 	@After
 	public void tearDown() {
 		PMClient client = new PMClient();
@@ -27,6 +35,10 @@ public class Tests {
 		}
 	}
 	
+	/**
+	 * Method tests POST function. 
+	 * Once tasks is posted into server this method invokes all tasks and checks if posted task is saved on origin server
+	 */
 	@Test
 	public void postOneTaskAndCheckIfServerNotNull() {	
 		PMClient client = new PMClient();
@@ -34,13 +46,17 @@ public class Tests {
 		List<Task> tasks = client.getAllTasks();
 		assertNotNull(tasks);
 	}
-	
+	/**
+	 * Method creates a task and is testing if can be saved on origin server.
+	 */
 	@Test
 	public void setOneTaskAndgetResponse() {
 		PMClient client = new PMClient();
 		assertNotNull(client.postTask(taskTest));
 	}
-	
+	/**
+	 * Method posts task into server and subsequently check each task's attribute 
+	 */
 	@Test
 	public void postOneTaskAndCheckIfDataCorrect() {
 		PMClient client = new PMClient();
@@ -55,7 +71,9 @@ public class Tests {
 		assertEquals(t.getPriority(), Priority.WYSOKI);
 		assertTrue(t.isFinishStatus());
 	}
-	
+	/**
+	 * Method is testing if attributes of posted task into origin server can be invoked/retrieved.
+	 */
 	@Test
 	public void postOneTaskGetPostedTaskAndCheckIfDataCorrect() {
 		PMClient client = new PMClient();
@@ -72,7 +90,9 @@ public class Tests {
 		assertTrue(tp.isFinishStatus());
 	}
 	
-	
+	/**
+	 * Method is testing if PUT function is working properly by editing task's attributes and then invoking them
+	 */
 	@Test
 	public void postOneTaskEditTaskAndCheckIfDataCorrect() {
 		PMClient client = new PMClient();
@@ -89,7 +109,9 @@ public class Tests {
 		assertEquals(t.getPriority(), Priority.WYSOKI);
 		assertTrue(t.isFinishStatus());
 	}
-	
+	/**
+	 * Method is testing if PUT function is working properly by editing task's attributes and then invoking them
+	 */
 	@Test
 	public void postOneTaskEditGetPostedTaskAndCheckIfDataCorrect() {
 		PMClient client = new PMClient();
@@ -107,7 +129,10 @@ public class Tests {
 		assertEquals(t.getPriority(), Priority.WYSOKI);
 		assertTrue(t.isFinishStatus());
 	}
-	
+	/**
+	 * Method is checking if POST and DELETE functions are working correctly 
+	 * @return null 
+	 */
 	@Test
 	public void postOneTaskDeleteAndCheckIfNull() {
 		PMClient client = new PMClient();
